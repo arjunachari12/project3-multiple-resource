193f577158dcb9b6b39ab9274f7015cd27c4e3aa
arjun <arjun.achari12@gmail.com>
2023-05-25 06:02:09 +0000

    sa

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "azurerm_resource_group" "rg" {
     name = local.resource_group_name
     location = local.location
 }
+
+resource "azurerm_storage_account" "storage" {
+
+  name                     = "arjunfdsdsafd3234"
+  resource_group_name      = local.resource_group_name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+}
